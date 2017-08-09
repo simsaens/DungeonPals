70e2299e4b32644b079b5ad7b5158ed51e59e68b
Simeon Saëns <simeon@twolivesleft.com>
2017-08-10 00:59:10 +0930

    Initial Commit

---
+++ Dungeon Contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dungeon Contacts
+//
+//  Created by Simeon on 10/8/17.
+//  Copyright © 2017 Two Lives Left. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
